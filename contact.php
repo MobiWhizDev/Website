f1f3ef34b07531531c293cd705ad2b2227839412
syed maaz <maaz@mobiwhiz.com>
2015-12-23 16:20:48 +0500

    indentation for all files

--- contact.php
+++ contact.php
@@ -1,71 +1,56 @@
 <?php include_once('includes/header.php'); ?>
-  <div class="contact">
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img src="images/bg-contact.jpg" class="img-slider" alt="...">
-          <div class="carousel-caption">
-              <!-- Capiton 1 Start -->
-              <h2> Talk to Us <br>About Your Project</h2>
-              <h4>Our App Strategist will develop the best app strategy and App Designs Free of Cost for you</h4>
-              <br> <br>
-              <a href="#contactform"><button class="button"> GET STARTED <img src="images/icon-button.png" class="icon-button"> </button></a>
-           
-              <!-- Caption 1 End -->
-          </div>
-        </div>
-        
-      </div>
-    </div>
-
-<!-- Home slider end -->
-
-<!-- secction contact start -->
-<section class="section_contact" id="contactform">
-			
-		<?php include('includes/contact_form.php'); ?>
-		<!--  container end  -->
-
-</section>
-<!-- section contact end -->
-
-
-<!-- Section Services Start -->
-<section class="section_services map">
-	
-    <div class="container">
-    	
-        <div class="contact">
-        	<h3> CONTACT DETAILS </h3>
-            
-            <div class="col-md-6">
-            	<br><br>
-            	<img src="images/address.png">
-                <br>
-                <h5>ADDRESS</h5>
-                668 Stony Hill Road, Suite 11,<br>
-                Yardley, PA 19067<br>
-                <a href="mailto:info@mobiwhiz.com">info@mobiwhiz.com</a>
+    <div class="contact">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="images/bg-contact.jpg" class="img-slider" alt="...">
+                    <div class="carousel-caption">
+                        <!-- Capiton 1 Start -->
+                        <h2> Talk to Us <br>About Your Project</h2>
+                        <h4>Our App Strategist will develop the best app strategy and App Designs Free of Cost for you</h4>
+                        <br>
+                        <br>
+                        <a href="#contactform">
+                            <button class="button"> GET STARTED <img src="images/icon-button.png" class="icon-button"> </button>
+                        </a>
+                        <!-- Caption 1 End -->
+                    </div>
+                </div>
             </div>
-            
-            <div class="col-md-6">
-            	<br><br>
-            	<img src="images/call3.png">
-                <br>
-                <H5>CALL US AT </H5>
-                Tel:(+1) 267-933-0030
-            </div>
-
-        
         </div>
+        <!-- Home slider end -->
+        <!-- secction contact start -->
+        <section class="section_contact" id="contactform">
+            <?php include('includes/contact_form.php'); ?>
+                <!--  container end  -->
+        </section>
+        <!-- section contact end -->
+        <!-- Section Services Start -->
+        <section class="section_services map">
+            <div class="container">
+                <div class="contact">
+                    <h3> CONTACT DETAILS </h3>
+                    <div class="col-md-6">
+                        <br>
+                        <br>
+                        <img src="images/address.png">
+                        <br>
+                        <h5>ADDRESS</h5> 668 Stony Hill Road, Suite 11,
+                        <br> Yardley, PA 19067
+                        <br>
+                        <a href="mailto:info@mobiwhiz.com">info@mobiwhiz.com</a>
+                    </div>
+                    <div class="col-md-6">
+                        <br>
+                        <br>
+                        <img src="images/call3.png">
+                        <br>
+                        <H5>CALL US AT </H5> Tel:(+1) 267-933-0030
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Section Services End -->
     </div>
-	
-</section>
-<!-- Section Services End -->
-
-
-</div>
-
-
-<?php include_once('includes/footer.php'); ?>
+    <?php include_once('includes/footer.php'); ?>
